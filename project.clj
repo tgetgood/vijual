d492a76f90d8f0515493698dcfe247ffbbde4b97
Thomas Getgood <thomas.getgood@gmail.com>
2019-06-14 15:17:50 -0400

    Switch to deps.

--- project.clj
+++
@@ -1,4 +0,0 @@
-(defproject vijual "0.1.0-SNAPSHOT" 
-  :description "A Graph Layout Library For Clojure" 
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"] 
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
